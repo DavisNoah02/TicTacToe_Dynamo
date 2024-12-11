cfa65e276ee79c85889de77ddc8ac42cfb015fce
Noah_Dave <m.davenoa@gmail.com>
2024-12-11 21:00:34 +0300

    Backend init

---
+++ tic_tac_toe-Backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+//routes
+app.get('/', (req, res) => {
+    res.send('Tic Toe App api is running!');
+});
+
+//route for making moves
+app.post('./api/play', (req, res)=> {
+
+    const {board, player} = req.body;
+    //logic to update the board and check for winners
+    res.json({message: 'move recieved !', board,nextPlayer : player === 'x' ? O: 'x'});
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
